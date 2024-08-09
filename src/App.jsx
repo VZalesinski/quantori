70d88158ca16850959812cd7dbe198f83a83256f
vladyffka <vzalesinski@gmail.com>
2024-08-09 13:27:47 +0400

    feat: add shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return <Button>Hello world!</Button>
 }
