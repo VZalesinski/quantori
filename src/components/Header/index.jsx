21d8f1a8c6a4a3f7da627b47fdffd71308491d6e
vladyffka <vzalesinski@gmail.com>
2024-08-10 18:30:33 +0400

    feat: add avatar in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,18 @@ import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Menu } from '@/components/Menu'
 import { LoginButton } from '@/components/LoginButton'
+import useFetchUserData from '@/hooks/useFetchUserData'
 
 export const Header = () => {
+  const { data } = useFetchUserData()
+
   return (
     <header className='px-6 h-[72px] py-4 flex items-center justify-between bg-slate-500'>
       <div className='flex items-center gap-x-4'>
         <Avatar>
-          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarImage
+            src={data ? data.image : 'https://github.com/shadcn.png'}
+          />
         </Avatar>
 
         <Tabs defaultValue='home' className='w-[400px] hidden md:block'>
